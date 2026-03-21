098f5dbae5ea59662428d95f0a562f944e278d2e
Georgios_Kapsanakis <geokapscode@gmail.com>
2026-03-21 14:14:38 +0200

    Added more functionality in Master, ClientHandler & Worker.

--- src/src/worker/Worker.java
+++ src/src/worker/Worker.java
@@ -6,10 +6,47 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
-public class Worker implements Runnable {
+public class Worker {
 
+    private final int port;
+    private final HashMap<String, Game> games = new HashMap<>();
 
+    public Worker(int port) {
+        this.port = port;
+    }
 
-    public void run();
+    public void start() {
+        try (
+                ServerSocket serverSocket = new ServerSocket(port);
+                ) {
+            System.out.println("Worker server listening on port " + port);
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("[Worker:" + port + "] New connection from " + clientSocket.getInetAddress());
+
+                (new Thread(new WorkerHandler(clientSocket, this))).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public synchronized void addGame(Game game) {
+        games.put(game.getGameName(), game);
+        System.out.println("[Worker:" + port + "] Added game " + game.getGameName());
+    }
+
+    public synchronized void removeGame(String gameName) {
+        Game game = games.get(gameName);
+        if (game != null) {
+            game.setActive(false);
+            System.out.println("[Worker:" + port + "] Removed game " + game.getGameName());
+        }
+    }
+
+    public synchronized Game getGame(String gameName) {
+        return games.get(gameName);
+    }
 
 }
--- src/src/worker/WorkerHandler.java
+++ src/src/worker/WorkerHandler.java
@@ -1,4 +1,16 @@
 package worker;
 
-public class WorkerHandler {
+import java.net.Socket;
+
+public class WorkerHandler implements Runnable {
+
+    private Socket clientSocket;
+    private Worker worker;
+
+    public WorkerHandler(Socket clientSocket, Worker worker) {
+        this.clientSocket = clientSocket;
+        this.worker = worker;
+    }
+
+    public
 }
